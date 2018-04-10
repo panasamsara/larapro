13a5850b1d85ab9262fa44b94baeaf2dc124358b
panasamsara <panasamsara@126.com>
2018-04-10 10:58:26 +0800

    重新踩坑lara5.5，新建了模型控制器和数据迁移，前端还没开始，先学vue

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,4 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/index', 'ArticleController@index');
