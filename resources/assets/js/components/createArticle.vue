d9c7d89ce3c01949503e6f2d8b3207745a93ac27
joshua <panasamsara@126.com>
2018-05-06 15:30:48 +0800

    返回按钮

--- resources/assets/js/components/createArticle.vue
+++ resources/assets/js/components/createArticle.vue
@@ -13,7 +13,7 @@
 
           <el-form-item>
             <el-button type="primary" @click="onSubmit">立即发布</el-button>
-            <el-button>取消</el-button>
+            <el-button @click='$router.history.go(-1)'>取消</el-button>
           </el-form-item>
         </el-form>
 
