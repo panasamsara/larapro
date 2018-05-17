b56dd65559428bbd77ec56ea0d34182f8d05d1e1
panasamsara <panasamsara@126.com>
2018-05-17 11:57:04 +0800

    将图片上传从编辑组件抽离出来，成单独的组件

--- resources/assets/js/components/articleEdit.vue
+++ resources/assets/js/components/articleEdit.vue
@@ -7,23 +7,15 @@
 
        <el-button  @click='$router.history.go(-1)'>返回</el-button >
        <el-button type="primary" @click='update(article.data.id)'>保存</el-button>
-        <div class='upload-box'>
-            <div class="form-group">
-                <h3>上传封面图片（尺寸：300 x 180）</h3>
-                <input type="file" class="form-control"  @change="onFileChange">
-            </div>
-            <div class="form-group" v-if="image">
-                    <label>预览</label>
-                    <img :src="image"> 
-            </div>
-            <button type="button" v-if='showUploadBtn' class="btn btn-default" name="button" @click="uploadImg(article.data.id)"> 上传</button>
-        </div>
+       
+        <upload></upload>
        
     </div>
 </template>
 
 <script>
 import axios from 'axios'
+import upload from './upload'
     export default {
         data () {
             return {
@@ -42,16 +34,12 @@ import axios from 'axios'
                     name: "",
                     updated_at:""
                 },
-                image: '',
-                imgData: {  
-                    accept: 'image/gif, image/jpeg, image/png, image/jpg',  
-                },
-                showUploadBtn: false,
-                formData: {}
 
             }
         },
-
+        components: {
+            upload
+        },
         mounted() {
             this.getArticle()
         },
@@ -86,47 +74,7 @@ import axios from 'axios'
                   this.user = response.data
               })
             },
-            createImage(file) {
-                var image = new Image();
-                var reader = new FileReader();
-                var vm = this;
-                
-                reader.onload = (e) => {
-                    vm.image = e.target.result;
-                };
-                reader.readAsDataURL(file);
-                this.showUploadBtn = true
-            },
-            onFileChange(e){
-                let reader =new FileReader();  
-                let img=event.target.files[0]; 
-                this.createImage(img);
-
-                let type=img.type;//文件的类型，判断是否是图片  
-                let size=img.size;//文件的大小，判断图片的大小  
-                if(this.imgData.accept.indexOf(type) == -1){  
-                    alert('请选择我们支持的图片格式！');  
-                    return false;  
-                }  
-                if(size>3145728){  
-                    alert('请选择3M以内的图片！');  
-                    return false;  
-                }  
-
-                let form = new FormData();   
-                form.append('imageUpload', img, img.name);  
-                this.formData = form;
-
-            },
-            uploadImg(article_id){
-                this.$ajax.post('upload/'+article_id,this.formData,{  
-                    headers:{'Content-Type':'multipart/form-data'}  
-                }).then(response => {  
-                    this.showUploadBtn = false
-                }).catch(error => {  
-                    console.log('上传图片出错！');  
-                })  
-            }
+            
         }
     }
 </script>
@@ -137,7 +85,5 @@ import axios from 'axios'
 .text-box textarea{
     height: 120px;
 }
-.upload-box{
-    margin-top: 20px;
-}
+
 </style>
