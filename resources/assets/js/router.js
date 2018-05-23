f1c0e7b36796e39f797cfa1f20331d4c14d57adb
panasamsara <panasamsara@126.com>
2018-05-23 15:03:47 +0800

    加入nprogress，路由全局守卫加入该动画

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 // 引入组件
 // import home from "./components/home";
@@ -52,4 +54,12 @@ const routes = [
 var router =  new VueRouter({
     routes
 })
+router.beforeEach((to, from, next) => {
+    NProgress.start();
+    next();
+})
+router.afterEach((to, from) => {
+    NProgress.done();
+})
+
 export default router;
